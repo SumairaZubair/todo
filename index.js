add85730d118f21a5eced18940cb79675f939c05
SumairaZubair <sumairazubair758@gmail.com>
2023-06-06 10:37:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,101 @@
+
+// const item= document.querySelector("#item")
+// const toDoBox= document.querySelector("#to-do-box")
+
+// item.addEventListener(
+//     "keyup",
+//     function(event){
+//         if (event.key =="Enter"){
+//             addToDo(this.value)
+//             this.value= ""
+//         }
+//     }
+// )
+
+// const addToDo = (item) => {
+//     const listItem =document.createElement("li");
+//     listItem.innerHTML= `
+//     ${item}
+//     // <i class="fa fa-close" style="font-size:20px"></i>
+//     `;
+
+//     listItem.addEventListener(
+//         "click",
+//         function(){
+//             this.classList.toggle("done")
+//         }
+//     )
+
+//     listItem.querySelector("i").addEventListener(
+//         "click",
+//         function(){
+//             listItem.remove()
+//         }
+//     )
+//     toDoBox.appendChild(listItem)
+// }
+
+
+//   calculator
+
+
+
+// function calculator(){
+
+//   var opr = prompt("enter your operator");
+//   // var result = (input,input2,opr);
+//   if(opr === "+"){
+//     document.write(input+input2)
+//   }
+// }
+// calculator()
+// add
+// var num1 = Number(prompt("enter your number"))
+// var num2 = Number(prompt("enter your number"))
+// var opr = prompt("enter your operator")
+// te("<br />")
+
+// mud//
+//  function add(num1,num2){
+//       return num1 + num2
+//      }
+// console.log(add(num1,num2))
+
+// function sub(num1,num2){
+//   return num1 - num2
+//  }
+// console.log(sub(num1,num2))
+
+
+// function mud(num1,num2){
+//   return num1 % num2
+//  }
+// console.log(mud(num1,num2))
+
+
+// // div
+// function div(num1,num2){
+//   return num1 /  num2
+//  }
+// console.log(div(num1,num2))
+
+// function funky(num1,num2,num3){
+//   return num2*num3 + num1 
+// }
+// console.log(funky(num1,num2,num3))
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
